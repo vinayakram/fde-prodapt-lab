516c3327fb938c51b10ddab44f14ed40eac99afa
Tamizhvendan S <tamizh88@gmail.com>
2025-11-26 12:08:34 +0530

    fix css

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router"
-import "./app.css"
+import appStylesHref from "./app.css?url"
+
 export default function App() {
   return (
     <html>
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href={appStylesHref} />
         <title>Jobify</title>
       </head>
       <body>
